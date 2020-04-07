21c36460cc2609982a02ffb4da8dcf6f732ddeb8
ezaksch <akshaychauhanjp@gmail.com>
2020-04-08 01:11:47 +0530

    Angular project

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Course} from '../course';
+import {Courses} from '../mock-courses';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  //course : Course={id:1,name:"Angular"}
+  courses = Courses;
+  selectCourse:Course;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(course:Course):void{
+    this.selectCourse=course;
+  }
+}
